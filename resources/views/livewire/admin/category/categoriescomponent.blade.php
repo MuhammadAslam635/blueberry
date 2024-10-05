6937f62b691f78b7ef50de1a431b7bf6e6dab8ca
MuhammadAslam635 <muhammadaslamkhakh395@gmail.com>
2024-10-05 16:29:24 -0700

    Build

--- resources/views/livewire/admin/category/categoriescomponent.blade.php
+++ resources/views/livewire/admin/category/categoriescomponent.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="flex justify-between py-3">
+    <div class="flex justify-between lg:py-5 sm:py-1 md:py-1">
         <h1 class="font-bold text-primary text-md">Category Section</h1>
         <x-mary-button label="Create Category" link="{{ route('createCategory') }}" class="btn btn-sm text-primary" />
     </div>
